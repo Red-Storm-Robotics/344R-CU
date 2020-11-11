acd9b3c5e4f7b0e7b45ad23acd423fcbf5d373af
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-11-10 20:12:42 -0500

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,96 @@
+#include "main.h"
+
+Controller controller;
+ControllerButton intake_fwd(ControllerDigital::L1);
+ControllerButton intake_rev(ControllerDigital::L2);
+ADIButton ball_detect('A');
+
+auto drive = ChassisControllerBuilder().withMotors(1, -2, -10, 9).withDimensions(AbstractMotor::gearset::green, {{4_in, 14_in}, imev5GreenTPR}).build();
+
+MotorGroup intake({11, -12});
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+	pros::lcd::initialize();
+	pros::lcd::set_text(0, "344R Code");
+}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+
+int intake_state = 0;
+
+void opcontrol() {
+
+	while (true) {
+		drive->getModel()->arcade(controller.getAnalog(ControllerAnalog::leftY),
+                              controller.getAnalog(ControllerAnalog::rightX));
+
+		if (ball_detect.changedToPressed()) {
+			if (intake_state > 0) {
+				intake_state = 0;
+			}
+		} else if (intake_fwd.changedToPressed()) {
+			if (intake_state > 0) {
+				intake_state = 0;
+			} else {
+				intake_state = 12000;
+			}
+		} else if (intake_rev.changedToPressed()) {
+			if (intake_state < 0) {
+				intake_state = 0;
+			} else {
+				intake_state = -12000;
+			}
+		}
+		intake.moveVoltage(intake_state);
+		pros::delay(10);
+	}
+}
